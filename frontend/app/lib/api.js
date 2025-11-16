8e93e10fb3d4b9322fd1dde7152c4b11a0420d47
Yaman Khan <khanyaman2003@gmail.com>
2025-11-16 16:35:47 +0530

    Update README and project changes

--- frontend/app/lib/api.js
+++ frontend/app/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000", // your backend
+    baseURL: "https://hackathon-yaman.onrender.com/", // your backend
     withCredentials: true,
 });
 
